99f9410d8c858fe6e89212574c81bfc3aedef849
AlphaSimpli <ousm0070@gmail.com>
2025-05-11 14:13:42 -0400

    message

---
+++ code.py
@@ -0,0 +1,8 @@
+def world_of_functions():
+    print("Diallo dit : Apprends chaque jour !")
+    print("Fatima dit : Python c’est cool !")
+    print("Ali dit : Ne jamais abandonner.")
+    # Chaque participant peut ajouter sa ligne ici
+    print("TonNom dit : Ton message ici")
+
+world_of_functions()

6d3a7e6e56ff9daa9f03857ec355eb95a0b331d5
Balde-ui <hssanebalde98@gmail.com>
2025-05-11 19:26:43 +0000

    Ajout de ma ligne

--- code1.py
+++ code1.py
@@ -2,6 +2,7 @@ def world_of_functions():
     print("Diallo dit : Apprends chaque jour !")
     print("Fatima dit : Python c’est cool !")
     print("Ali dit : Ne jamais abandonner.")
+    print("Hassan dit: Coder en s'amusant !")
     # Chaque participant peut ajouter sa ligne ici
     print("TonNom dit : Ton message ici")
 
